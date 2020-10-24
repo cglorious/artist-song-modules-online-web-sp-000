f3a054b4a56a39f04871e3e78fbabd257d2a6fda
Catherine Gloria <catherinegloria@gmail.com>
2020-10-24 14:36:48 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -3,7 +3,7 @@ require 'pry'
 class Song
 
   extend Memorable
-  extend Findable
+  #extend Findable
 
   attr_accessor :name
   attr_reader :artist
@@ -14,9 +14,9 @@ class Song
     @@songs << self
   end
 
-  #def self.find_by_name(name)
-  #  @@songs.detect{|a| a.name == name}
-  #end
+  def self.find_by_name(name)
+    @@songs.detect{|a| a.name == name}
+  end
 
   def self.all
     @@songs
